e8e2b2968eb76451c19463aaa56492a0cbaba251
Anthony Pena <anthony.pena@outlook.fr>
2019-04-18 23:21:04 +0200

    strapi policies for simple collection owner management

---
+++ back/strapi/api/collection/config/policies/isOwner.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * `isOwner` policy.
+ */
+
+module.exports = async (ctx, next) => {
+  const { role, id } = ctx.state.user;
+
+  // defined only when a specific collection is ask
+  const fieldId = ctx.params.id;
+
+  if (typeof fieldId !== "undefined") {
+    Collection.fetchAll({ id: fieldId, owner: id }).then(result => {
+      if (!result && role.type !== "administrator") {
+        return ctx.unauthorized("You are not allowed to perform this action.");
+      }
+    });
+  } else {
+    if(typeof ctx.query.owner === 'undefined' && role !== "administrator") {
+       ctx.query.owner = id;
+    }
+  }
+
+  await next();
+};
