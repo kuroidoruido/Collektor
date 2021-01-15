2b5b10b8141a79a53171f005b73dd4c325abcf00
Anthony Pena <anthony.pena@outlook.fr>
2021-01-15 23:06:50 +0100

    not found page + image lazy loading

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,16 +4,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { CollektionGridModule, CollektionGridComponent } from './collektion-grid';
 import { EditCollektionModule, AddCollektionComponent, EditCollektionComponent } from './edit-collektion';
 import { ItemGridModule, ItemGridComponent } from './item-grid';
+import { NotFoundComponent, NotFoundModule } from './not-found';
 
 const routes: Routes = [
   { path: '', component: CollektionGridComponent, pathMatch: 'full'},
   { path: 'collection/add', component: AddCollektionComponent},
   { path: 'collection/:collektionId', component: ItemGridComponent},
   { path: 'collection/edit/:collektionId', component: EditCollektionComponent},
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), CollektionGridModule, EditCollektionModule, ItemGridModule],
+  imports: [RouterModule.forRoot(routes), NotFoundModule, CollektionGridModule, EditCollektionModule, ItemGridModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
