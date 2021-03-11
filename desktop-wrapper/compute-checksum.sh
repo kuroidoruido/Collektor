5c86948b88b32e97fc3ee9e2d5e345e0de29e75a
Anthony Pena <anthony.pena@outlook.fr>
2021-03-11 22:49:27 +0100

    fix - build pipeline

--- desktop-wrapper/compute-checksum.sh
+++ desktop-wrapper/compute-checksum.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sha256sum out/make/deb/x64/collektor_*_amd64.deb > $(/bin/ls out/make/deb/x64/).sha256
-sha256sum out/make/rpm/x64/collektor_*_amd64.rpm > $(/bin/ls out/make/rpm/x64/).sha256
-sha256sum out/make/zip/x64/collektor_*_amd64.zip > $(/bin/ls out/make/zip/x64/).sha256
+rm *.sha256
+sha256sum out/make/deb/x64/collektor_*.deb > $(/bin/ls out/make/deb/x64/).sha256
+sha256sum out/make/rpm/x64/collektor-*.rpm > $(/bin/ls out/make/rpm/x64/).sha256
+sha256sum out/make/zip/linux/x64/collektor-*.zip > $(/bin/ls out/make/zip/linux/x64/).sha256
